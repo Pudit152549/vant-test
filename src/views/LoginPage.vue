2e6df584d6e64aab7acc5c5d9dc82718ee52f8d5
Pudit152549 <67319010027@technicrayong.ac.th>
2025-09-23 14:25:06 +0700

    Add LoginPage component and integrate Vant UI library
    - Created LoginPage.vue for user authentication
    - Updated App.vue to use LoginPage
    - Modified main.ts to include Vant UI
    - Updated package.json and package-lock.json with new dependencies

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { showToast } from 'vant'
+
+const username = ref('')
+const password = ref('')
+const active = ref(0)
+
+const onSubmit = (values: any) => {
+  console.log('submit', values)
+}
+const onClickLeft = () => history.back()
+
+// NumberKeyboard
+const show = ref(false)
+const onInput = (value: string) => {
+  showToast(value)
+}
+const onDelete = () => {
+  showToast('delete')
+}
+</script>
+
+<template>
+  <div class="max-w-md mx-auto min-h-dvh flex flex-col bg-white">
+    <!-- NavBar -->
+    <van-nav-bar
+      fixed
+      placeholder
+      title="Login"
+      left-text="Back"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+
+    <!-- Content -->
+    <main class="flex-1 px-4 pt-3 pb-[calc(16px+env(safe-area-inset-bottom))]">
+      <h2 class="text-3xl font-bold text-blue-500">Welcome to Login Page</h2>
+
+      <van-form @submit="onSubmit" class="flex flex-col">
+        <van-cell-group inset>
+          <van-field
+            v-model="username"
+            name="Username"
+            label="Username"
+            placeholder="Username"
+            :rules="[{ required: true, message: 'Username is required' }]"
+          />
+          <van-field
+            v-model="password"
+            type="password"
+            name="Password"
+            label="Password"
+            placeholder="Password"
+            :rules="[{ required: true, message: 'Password is required' }]"
+          />
+        </van-cell-group>
+
+        <!-- ปุ่มโชว์คีย์บอร์ด -->
+        <van-cell title="Show Keyboard" is-link @click="show = true" />
+
+        <!-- Number keyboard -->
+        <van-number-keyboard
+          :show="show"
+          close-button-text="Close"
+          @blur="show = false"
+          @input="onInput"
+          @delete="onDelete"
+        />
+
+        <!-- Submit button -->
+        <div class="mt-4">
+          <van-button round block type="primary" native-type="submit">
+            Submit
+          </van-button>
+        </div>
+      </van-form>
+    </main>
+
+    <!-- Tabbar -->
+    <van-tabbar v-model="active" fixed safe-area-inset-bottom>
+      <van-tabbar-item icon="home-o">Tab</van-tabbar-item>
+      <van-tabbar-item icon="search">Tab</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">Tab</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">Tab</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
