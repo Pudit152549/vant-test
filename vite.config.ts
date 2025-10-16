0d5129cf5680907772c300298812482ca18bfdb4
67319010027@technicrayong.ac.th <67319010027@technicrayong.ac.th>
2025-10-16 10:50:14 +0700

    chore: update dependencies and configure auto-import for Vant components
    - Added @vant/auto-import-resolver, unplugin-auto-import, unplugin-vue-components, and vite-plugin-vue-devtools to devDependencies in package.json.
    - Updated tsconfig.app.json to include baseUrl and paths for module resolution.
    - Configured Vite to use auto-import and component resolvers for Vant in vite.config.ts.
    - Generated auto-imports.d.ts and components.d.ts for automatic imports and component declarations.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from '@vant/auto-import-resolver'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),  
-  ], 
-  base: '/'
+    tailwindcss(),
+    AutoImport({
+      resolvers: [VantResolver()],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      resolvers: [VantResolver()],
+      dts: 'src/components.d.ts',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
